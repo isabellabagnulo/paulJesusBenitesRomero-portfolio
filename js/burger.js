6683864eae0ec7208e71809825d16f6a997876f0
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2021-11-04 12:54:29 +0100

    accordion non funziona

---
+++ js/burger.js
@@ -0,0 +1,34 @@
+function burgermenu() {
+
+    const menu = document.querySelector('.menu');
+    const burger = document.querySelector('.burger-menu');
+    const open = document.querySelector('.open');
+    const close = document.querySelector('.close');
+    
+    burger.addEventListener("click", toggleMenu);
+    function toggleMenu() {
+        if (menu.classList.contains("expanded")) {
+            menu.classList.remove("expanded");
+            setTimeout(function(){
+                close.style.display = "none";
+                open.style.display = "block";
+            }, 300);
+        } else {
+            menu.classList.add("expanded");
+            close.style.display = "block";
+            open.style.display = "none";
+        }
+    }
+    
+    const menuLinks = document.querySelectorAll('.menu-link');
+    menuLinks.forEach(
+        function(menuLink) {
+            menuLink.addEventListener("click", toggleMenu);
+        }
+    )
+
+}
+
+export{
+    burgermenu
+}
